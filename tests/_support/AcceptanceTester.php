6e1090490272da36c0932cdcd62d8112fd934fc5
Yngve Høiseth <yhoiseth@gmail.com>
2017-06-10 14:59:10 +0200

    Codeception (#1)
    * Install Codeception
    
    * Bootstrap Codeception
    
    * Add JavaScript execution dummy test
    
    * Require PHP 7.1 or greater
    
    * Make JS test fail
    
    * Fix JS test
    
    * Debug CI
    
    * Debug CI
    
    * Debug CI

---
+++ tests/_support/AcceptanceTester.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\AcceptanceTesterActions;
+
+    /**
+     * @Then I should see :arg1
+     */
+    public function iShouldSee($arg1)
+    {
+        $this->amOnPage('/');
+        $this->canSee($arg1);
+    }
+}
