a9549dd05e78478a07d874f82199dc89b89a205b
Yngve Høiseth <yhoiseth@gmail.com>
2017-07-27 20:28:29 +0200

    Cleanup

--- src/AppBundle/Service/VersionControlSystem.php
+++ src/AppBundle/Service/VersionControlSystem.php
@@ -10,25 +10,15 @@ class VersionControlSystem
     /** @var string $collectionsDirectory */
     private $collectionsDirectory;
 
-    /** @var TokenStorageInterface $tokenStorage */
-    private $tokenStorage;
-
-    public function __construct(string $collectionsDirectory, TokenStorageInterface $tokenStorage)
+    public function __construct(string $collectionsDirectory)
     {
         $this->setCollectionsDirectory($collectionsDirectory);
-        $this->setTokenStorage($tokenStorage);
     }
 
     public function initializeRepository(string $username)
     {
         $collectionsDirectory = $this->getCollectionsDirectory();
-//        $username = $this->getTokenStorage()->getToken()->getUsername();
-
-//        dump($collectionsDirectory);
-//        dump($username);
-//        die;
-
-
+        
         exec("git init $collectionsDirectory/$username");
     }
 
@@ -46,21 +36,5 @@ class VersionControlSystem
     public function setCollectionsDirectory(string $collectionsDirectory)
     {
         $this->collectionsDirectory = $collectionsDirectory;
-    }
-
-    /**
-     * @return TokenStorageInterface
-     */
-    public function getTokenStorage(): TokenStorageInterface
-    {
-        return $this->tokenStorage;
-    }
-
-    /**
-     * @param TokenStorageInterface $tokenStorage
-     */
-    public function setTokenStorage(TokenStorageInterface $tokenStorage)
-    {
-        $this->tokenStorage = $tokenStorage;
     }
 }
