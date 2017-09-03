a9e4fb9748b0e6fd941f9bfa9fdb8ff26457e624
Yngve Høiseth <yngve@hoiseth.net>
2017-09-03 14:15:58 +0200

    Fix input focus issue on edit page

---
+++ web/assets/js/text/edit.js
@@ -0,0 +1,61 @@
+// var $ = require('jquery');
+//
+// require('bootstrap-sass');
+
+require('./../main');
+
+$(document).ready(function() {
+  var $wrapper = $('#js-editor');
+  var $form = $wrapper.find('form');
+  var $statusText = $('#js-status-text-container');
+  var $textarea = $wrapper.find('textarea');
+  var timeoutId;
+
+  $textarea
+    .on(
+      'input propertychange change selectionchange',
+      function() {
+        $statusText.html('Saving draft…');
+
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function() {
+
+          $.ajax({
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize()
+          })
+            .done(function(response) {
+              $statusText.html('Draft saved');
+            })
+          ;
+        }, 1000);
+      }
+    )
+  ;
+
+  var $saveTextBodyButton = $('#js-save-text-body-button');
+
+  $saveTextBodyButton
+    .on(
+      'click',
+      function() {
+        $(this).prop('disabled',true);
+
+        $.ajax({
+          type: 'POST',
+          url: window.location.href,
+          data: {
+            'save': true
+          }
+        })
+          .done(function(response) {
+            if (response === 'text successfully saved') {
+              window.location.href = $saveTextBodyButton.data('url');
+            }
+          })
+        ;
+
+      }
+    )
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ Encore
     'app',
     './web/assets/js/main.js'
   )
+  .addEntry(
+    'app_text_edit',
+    './web/assets/js/text/edit.js'
+  )
   .addStyleEntry(
     'global',
     './web/assets/css/global.scss'
