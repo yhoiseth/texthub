a09f6d3ae1032459209e6c0848afd48c464c6e0b
Yngve Høiseth <yngve@hoiseth.net>
2017-09-09 14:04:19 +0200

    Upload built assets

--- deploy.php
+++ deploy.php
@@ -32,6 +32,36 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
+task('deploy:assets:upload', function () {
+    run('yarn run encore production');
+    run('rsync -avzp web/build/ texthub@139.59.161.132:deployer-test/release/web/build');
+})->local();
+
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:create_cache_dir',
+    'deploy:shared',
+    'deploy:assets',
+    'deploy:vendors',
+    'deploy:assets:install',
+    'deploy:assetic:dump',
+    'deploy:assets:upload',
+    'deploy:cache:clear',
+    'deploy:cache:warmup',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy your project');
+
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
